7791f5ad170f6ad29e00afdb43e35a23f09bd261
immi <immi>
2016-07-16 16:19:15 +0530

    jul163

--- common/models/chat-user.js
+++ common/models/chat-user.js
@@ -1,20 +1,20 @@
 module.exports = function(ChatUser) {
-  ChatUser.joinroom = function(room_id,user_id, cb) {
-    ChatUser.app.models.Room.findById(room_id,function(err,room){
-      ChatUser.findById(user_id,function(err,data){
-        data.joinedrooms.add(room,function(err,res){
-          cb();
-        });
-      });
-    });
-  };
-
-  ChatUser.remoteMethod('joinroom', {
-    accepts: [
-      {arg: 'room_id', type: 'string'},
-      {arg: 'user_id', type: 'string'}
-    ],
-    returns: {arg: 'success', type: 'boolean'},
-    http: {path:'/joinroom', verb: 'post'}
-  });
+  //ChatUser.joinroom = function(room_id,user_id, cb) {
+  //  ChatUser.app.models.Room.findById(room_id,function(err,room){
+  //    ChatUser.findById(user_id,function(err,data){
+  //      data.joinedrooms.add(room,function(err,res){
+  //        cb();
+  //      });
+  //    });
+  //  });
+  //};
+  //
+  //ChatUser.remoteMethod('joinroom', {
+  //  accepts: [
+  //    {arg: 'room_id', type: 'string'},
+  //    {arg: 'user_id', type: 'string'}
+  //  ],
+  //  returns: {arg: 'success', type: 'boolean'},
+  //  http: {path:'/joinroom', verb: 'post'}
+  //});
 };
