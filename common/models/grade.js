3f313fc706216fe97fb42644acaf5c08bb13e680
immi <immi>
2016-08-01 13:44:19 +0530

    aug12

--- common/models/grade.js
+++ common/models/grade.js
@@ -1,3 +1,8 @@
 module.exports = function(Grade) {
+    Grade.validatesUniquenessOf('gradeName', {message: 'Grade Already Exists'});
+    Grade.validatesUniquenessOf('gradePoint', {message: 'Grade Point Already Exists'});
+    Grade.validatesUniquenessOf('percentageRangeFrom', {message: 'This Grade Range From Limit Already Exists'});
+    Grade.validatesUniquenessOf('percentageRangeTo', {message: 'This Grade Range To Limit Already Exists'});
+    
 
 };
