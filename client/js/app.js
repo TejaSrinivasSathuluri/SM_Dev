f8935ee2a1a2a10078d51e0853c95386cef6325f
immi <immi>
2016-08-09 10:29:34 +0530

    newrecord7

---
+++ client/js/app.js
@@ -0,0 +1,69 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app', [ 
+    'ui.router',
+    'lbServices',
+    'ngDialog',
+    'mgo-angular-wizard',
+    'ngScrollbar',
+    'ui.rCalendar',
+    'calendarDemoApp'
+  ])
+  .config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+    $stateProvider
+    .state('dashboard',             { url: '/dashboard',         templateUrl: 'views/dashboard.html',          controller: 'LandingPageController', authenticate: true})
+    .state('directory',             { url: '/directory',         templateUrl: 'views/directory.html',          controller: 'DirectoryController',   authenticate: true})
+    .state('class',                 { url: '/class',             templateUrl: 'views/class.html',              controller: 'ClassController',       authenticate: true})
+    .state('subject',               { url: '/subject',           templateUrl: 'views/subject.html',            controller: 'SubjectController',     authenticate: true})
+    .state('attendance',            { url: '/attendance',        templateUrl: 'views/attendance.html',         controller: 'AttendanceController',  authenticate: true})
+    .state('timetable',             { url: '/timetable',         templateUrl: 'views/timetable.html',          controller: 'TimetableController',   authenticate: true})
+    .state('transport',             { url: '/transport',         templateUrl: 'views/transport.html',          controller: 'TransportController',   authenticate: true})
+    .state('schedule',              { url: '/schedule',          templateUrl: 'views/schedule.html',           controller: 'ScheduleController',    authenticate: true})
+    .state('noticeboard',           { url: '/noticeboard',       templateUrl: 'views/noticeboard.html',        controller: 'NoticeboardController', authenticate: true})
+    .state('library',               { url: '/library',           templateUrl: 'views/library.html',            controller: 'LibraryController',     authenticate: true})
+    .state('assignment',            { url: '/assignment',        templateUrl: 'views/assignment.html',         controller: 'AssignmentController',  authenticate: true})
+    .state('holiday',               { url: '/schoolcalendar',    templateUrl: 'views/holiday.html',            controller: 'HolidayController',  authenticate: true})
+    .state('profile',               { url: '/profile',           templateUrl: 'views/profile.html',            controller: 'ProfileController',  authenticate: true})
+    .state('expensemanagement',     { url: '/expensemanagement', templateUrl: 'views/expensemanagement.html',  controller: 'ExpenseController',  authenticate: true})
+    .state('fees',                  { url: '/feepayments',       templateUrl: 'views/fee.html',                controller: 'FeeController',  authenticate: true})
+    .state('exams',                 { url: '/exams',             templateUrl: 'views/exams.html',              controller: 'ExamController',  authenticate: true})
+    .state('grades',                { url: '/grades',            templateUrl: 'views/grades.html',             controller: 'GradeController',  authenticate: true})
+    .state('marks',                 { url: '/marks',             templateUrl: 'views/marks.html',              controller: 'MarksController',  authenticate: true})
+    .state('bulk',                  { url: '/bulkuploads',       templateUrl: 'views/bulk.html',               controller: 'BulkController',  authenticate: true})
+
+      //--------------Auth Services
+      .state('index',        { url: '/index',      controller: 'LandingPageController'})
+      .state('email',         { url: '/email',  templateUrl: 'views/email.html',   controller: 'EmailController'     })
+      .state('login',         { url: '/login',  templateUrl: 'views/login.html',   controller: 'AuthLoginController' })
+      .state('login.admin',   { url: '/admin',  templateUrl: 'views/admin.html',   controller: 'AuthLoginController' })
+      .state('login.staff',   { url: '/staff',  templateUrl: 'views/staff.html',   controller: 'AuthLoginController' })
+      .state('login.parent',  { url: '/parent', templateUrl: 'views/parent.html',  controller: 'AuthLoginController' })
+      .state('login.student', { url: '/student',templateUrl: 'views/student.html', controller: 'AuthLoginController' })
+      //.state('signup',        { url: '/signup', templateUrl: 'views/signup.html',  controller: 'SignUpController  ' })
+      .state('logout',        { url: '/login',                                     controller: 'AuthLogoutController'})
+
+     $urlRouterProvider.otherwise('login/admin');
+    //  $locationProvider.html5Mode({ enabled: true, requireBase: false});
+     
+  })
+
+  .run(function($rootScope, $state,$window) {
+    $rootScope.$on('$stateChangeStart', function(event, next) {
+      // redirect to login page if not logged in
+
+      if (!$window.localStorage.getItem('user')) {
+        if (next.authenticate && !$rootScope.currentUser) {
+          event.preventDefault(); //prevent current page from loading
+          $rootScope.currentUser = null;
+          $rootScope.schoolName = null;
+          $window.localStorage.clear();
+          $state.go('login');
+        }
+      }
+      $rootScope.currentUser = $window.localStorage.getItem('user');
+    });
+  });
---
+++ client/js/services/auth.js
@@ -0,0 +1,178 @@
+// Copyright Study Monitor. 2016. All Rights Reserved.
+// Node module: Study Monitor
+
+angular
+  .module('app')
+  .factory('AuthServiceAdmin',   ['Admin',  '$q', '$rootScope', '$window','School',function(User,   $q,$rootScope,$window,School) {
+    function login(email, password) {
+      return User
+        .login({email: email, password: password})
+        .$promise
+        .then(function(response) {
+          $rootScope.currentUser = {
+            id: response.user.id,
+            tokenId: response.id,
+            user : response.user
+          };
+          console.log('Admin Logged In');
+          $window.localStorage.setItem('user',JSON.stringify(response.user));
+          var school = School.findById({id:response.user.schoolId},function(response){
+            $window.localStorage.setItem('school',JSON.stringify(response));
+          });
+        });
+    }
+
+    function logout() {
+      return User
+        .logout()
+        .$promise
+        .then(function() {
+          console.log('Admin Logged Out');
+          $rootScope.currentUser = null;
+          $window.localStorage.clear();
+        }, function (response) {
+          $rootScope.currentUser = null;
+          $window.localStorage.clear();
+          console.log('Admin ' + response.data.error.message);
+        });
+    }
+
+    function register(email, password) {
+      return User
+        .create({
+          email: email,
+          password: password
+        })
+        .$promise;
+    }
+
+    return {
+      login: login,
+      logout: logout,
+      register: register
+    };
+  }])
+  .factory('AuthServiceStudent', ['Student','$q', '$rootScope', '$window','School',function(User, $q,$rootScope,$window,School) {
+  function login(email, password) {
+    return User
+      .login({email: email, password: password})
+      .$promise
+      .then(function(response) {
+        $window.localStorage.setItem('user',JSON.stringify(response.user));
+        var school = School.findById({id:response.user.schoolId},function(){
+          $window.localStorage.setItem('school',JSON.stringify(response));
+
+        });
+      });
+  }
+
+  function logout() {
+    return User
+      .logout()
+      .$promise
+      .then(function() {
+        $rootScope.currentUser = null;
+        $rootScope.schoolName = null;
+        $window.localStorage.clear();
+      });
+  }
+
+  function register(email, password) {
+    return User
+      .create({
+        email: email,
+        password: password
+      })
+      .$promise;
+  }
+
+  return {
+    login: login,
+    logout: logout,
+    register: register
+  };
+}])
+  .factory('AuthServiceStaff',   ['Staff',  '$q', '$rootScope', '$window','School',function(Staff,   $q,$rootScope,$window,School) {
+  function login(email, password) {
+    return User
+      .login({email: email, password: password})
+      .$promise
+      .then(function(response) {
+        $rootScope.currentUser = {
+          id: response.user.id,
+          tokenId: response.id,
+          user : response.user
+        };
+        $window.localStorage.setItem('user',JSON.stringify(response.user));
+        var school = School.findById({id:response.user.schoolId},function(){
+          $rootScope.schoolName = school.schoolName;});
+      });
+  }
+
+  function logout() {
+    return User
+      .logout()
+      .$promise
+      .then(function() {
+        $rootScope.currentUser = null;
+        $rootScope.schoolName = null;
+        $window.localStorage.clear();
+      });
+  }
+
+  function register(email, password) {
+    return User
+      .create({
+        email: email,
+        password: password
+      })
+      .$promise;
+  }
+
+  return {
+    login: login,
+    logout: logout,
+    register: register
+  };
+}])
+  .factory('AuthServiceParent',  ['Parent', '$q', '$rootScope', '$window','School',function(Parent,  $q,$rootScope,$window,School) {
+  function login(email, password) {
+    return User
+      .login({email: email, password: password})
+      .$promise
+      .then(function(response) {
+        $rootScope.currentUser = {
+          id: response.user.id,
+          tokenId: response.id,
+          user : response.user
+        };
+        $window.localStorage.setItem('user',JSON.stringify(response.user));
+      });
+  }
+
+  function logout() {
+    return User
+      .logout()
+      .$promise
+      .then(function() {
+        $rootScope.currentUser = null;
+        $rootScope.schoolName = null;
+        $window.localStorage.clear();
+      });
+  }
+
+  function register(email, password) {
+    return User
+      .create({
+        email: email,
+        password: password
+      })
+      .$promise;
+  }
+
+  return {
+    login: login,
+    logout: logout,
+    register: register
+  };
+}]);
