4a1a5163f6df171f464952d85723dd9d22521eff
immi <immi>
2016-08-30 14:31:28 +0530

    newrecord21

--- server/boot/root.js
+++ server/boot/root.js
@@ -6,11 +6,20 @@
   remotes.after('find', function (ctx, next)  {  if (ctx.result) ctx.result.success = true; next();  });
   //----------------Modify Response
 
-  //          var Admin = server.models.Admin;
+           var Admin = server.models.Admin;
+           var Student = server.models.Student;
+           var Staff = server.models.Staff;
+          //  var Staff = server.models.Staff;
 
-  // Admin.afterRemote('login', function(ctx, unused, next) {
-  //  ctx.result.ttl = 10;   
-  //   next();
-  // });
+  //  var list = Admin.find();
+  //  console.log(list);
+  //  for (var i=0;i<list.length-1;i++){
+                 
+  //              console.log(list[i].email);               
+               
+  //              var email = list[i].email.toLowercase();
+  //              console.log(email);               
+       
+  //  }
   server.use(router);
 };
