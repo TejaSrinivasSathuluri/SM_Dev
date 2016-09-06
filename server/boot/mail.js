fa53c3ac52470257144964dfd5ba5345571c5b1f
immi <immi>
2016-09-06 14:40:59 +0530

    newrecord34

--- server/boot/mail.js
+++ server/boot/mail.js
@@ -45,30 +45,61 @@ module.exports = function(server) {
                 
             //     });
 
+                    var nodemailer = require('nodemailer');
+                    var smtpTransport = require('nodemailer-smtp-transport');
+                    var transporter = nodemailer.createTransport(smtpTransport({
+                        host: "smtp.gmail.com",
+                        port: 465,
+                        secure: true,
+                        auth: {
+                        user: 'mansoorshaik0201@gmail.com',
+                        pass: 'mannubh@i'
+                        }
+                    }));
 
-            var smtpTransport = nodemailer.createTransport("SMTP",{
-            service: "Gmail", 
-            auth: {
-                user: response.schoolEmail,
-                pass: response.schoolPassword
-            }
-            });
+                    var mailOptions = {
+                        from: "mansoorshaik0201@gmail.com",
+                        to: email,
+                        subject: "Welcome Spiceboxer", // Subject line
+                        text: '2324343', // plaintext body
+                        html: "Hi " + ctx.req.body.name + ", <br><br>We are glad to have a SpiceBoxer like you.You are successfully registered with us" + " <br><br> Team Spicebox"
+                    };
+
+                    transporter.sendMail(mailOptions, function (error, info) 
+                    {
+                        if (error) 
+                        {
+                        return console.log(error);
+                        }
+                        console.log('Message sent: ' + info.response);
+                    });
+
+// -----------------------------------------------------------------------------------------
+
+            // var smtpTransport = nodemailer.createTransport("SMTP",{
+            // service: "Gmail", 
+            // auth: {
+            //     user: response.schoolEmail,
+            //     pass: response.schoolPassword
+            // }
+            // });
 
 
 
-                        smtpTransport.sendMail(
-                            { 
-                                from: "<" + response.schoolEmail+ ">",
-                                to: "<" + email  + ">", 
-                                subject: subject, 
-                                text: message
-                            }, 
-                            function(error, response)
-                            {  
-                                    if(error) console.log(error);
-                                    else console.log("Message sent: " + response.message);
-                                    smtpTransport.close(); 
-                            });
+                        // smtpTransport.sendMail(
+                        //     { 
+                        //         from: "<" + response.schoolEmail+ ">",
+                        //         to: "<" + email  + ">", 
+                        //         subject: subject, 
+                        //         text: message
+                        //     }, 
+                        //     function(error, response)
+                        //     {  
+                        //             if(error) console.log(error);
+                        //             else console.log("Message sent: " + response.message);
+                        //             smtpTransport.close(); 
+                        //     });
+ // -------------------------------------------------------------------------------------
 
             });
         }
