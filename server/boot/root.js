d51fe458e1d4802d2fe85bad7280b370e286532b
immi <immi>
2016-09-22 12:34:29 +0530

    console2

--- server/boot/root.js
+++ server/boot/root.js
@@ -17,6 +17,6 @@
 // var c = new TMClient('username', 'C7XDKZOQZo6HvhJwtUw0MBcslfqwtp4');
 // c.Messages.send({text: 'test message', phones:'9705629205'}, function(err, res){
 //     console.log('Messages.send()', err, res);
-});
+// });
   server.use(router);
 };
